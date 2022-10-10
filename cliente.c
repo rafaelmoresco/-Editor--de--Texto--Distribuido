8bcdc9583a25b7797b09fc786f7c666d3ee62273
Rafael Moresco <rafaelmorescovieira@gmail.com>
2022-10-10 15:01:11 -0300

    no threading functional

--- cliente.c
+++ cliente.c
@@ -5,19 +5,20 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 int main()
 {
   int sockfd;
   int len;
   struct sockaddr_in address;
   int result;
-  char ch = 'A';
+  char op[8], ind[2], text[256], cmd[266];
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   address.sin_family = AF_INET;
-  //address.sin_addr.s_addr = inet_addr("127.0.0.1");
-  address.sin_addr.s_addr = inet_addr("150.162.65.154");
+  address.sin_addr.s_addr = inet_addr("127.0.0.1");
+  //address.sin_addr.s_addr = inet_addr("150.162.65.154");
   //address.sin_addr.s_addr = htonl(INADDR_ANY);
   address.sin_port = htons(9734);
 
@@ -27,10 +28,32 @@ int main()
 		perror("oops: client1");
 		exit(1);
   }
-	write(sockfd, &ch, 1);
-	//sleep(30);
-	read(sockfd, &ch, 1);
-	printf("char from server = %c\n", ch);
-	close(sockfd);
+	while (1) {
+    printf("Type Operation: ");
+    scanf("%s", &op);
+    printf("%s\n", &op);
+    if (strcmp(op, "add_line") == 0) {
+      printf("Type Line: ");
+      scanf("%s", &ind);
+      printf("Type Text:\n");
+      scanf("%s", &text);
+      strcpy(cmd, op); strcat(cmd, ind); strcat(cmd, text);
+      write(sockfd, &cmd, sizeof(cmd));
+    } else {
+      if (strcmp(op, "get_line") == 0) {
+        printf("Type Line: ");
+        scanf("%s", &ind);
+        strcpy(cmd, op); strcat(cmd, ind);
+        write(sockfd, &cmd, sizeof(cmd));
+        read(sockfd, &text, sizeof(text));
+        printf("%s\n", &text);
+      } else {
+        printf("Invalid Operation\n");
+        break;
+      }
+    }  
+  }
+  close(sockfd);
 	exit(0);
 }
+
--- servidor.c
+++ servidor.c
@@ -7,8 +7,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define STRING_SIZE 256
-#define NUM_STRINGS 32
+#define num_lines 32
+#define line_size 256
+#define max_clients 2
+
+pthread_t tid[max_clients];
+pthread_mutex_t cmd_lock;
+pthread_mutex_t doc_lock;
+pthread_mutex_t rtn_lock;
+
+void* user();
 
 int main()
 {
@@ -16,13 +24,15 @@ int main()
   int server_len, client_len;
   struct sockaddr_in server_address;
   struct sockaddr_in client_address;
-  char document[STRING_SIZE][NUM_STRINGS];
-  char cmd;
-
+  char document[num_lines][line_size];
+  char cmd[10+line_size];
+  char rtn[line_size];
+  pthread_mutex_init(&cmd_lock, NULL);
+  pthread_mutex_init(&doc_lock, NULL);
+  pthread_mutex_init(&rtn_lock, NULL);
+  
   server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
   server_address.sin_family = AF_INET;
-  //server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-  //server_address.sin_addr.s_addr = inet_addr("192.168.0.15");
   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
   server_address.sin_port = htons(9734);
   server_len = sizeof(server_address);
@@ -32,9 +42,37 @@ int main()
 		printf("server waiting\n");
 		client_len = sizeof(client_address);
 		client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_address, &client_len);
-		read(client_sockfd, &cmd, 1);
-		cmd++;
-		write(client_sockfd, &cmd, 1);
-		close(client_sockfd);
+		pthread_mutex_lock(&cmd_lock);
+    read(client_sockfd, &cmd, sizeof(cmd));
+		if (cmd[0] == 'a') {
+      int line_index = (cmd[8]-'0')*10 + (cmd[9]-'0');
+      pthread_mutex_lock(&doc_lock);
+      for (int i = 0; i < 256; i++) {
+        document[line_index][i] = cmd[9+i];
+        if (cmd[9+i] == '\0') {
+          break;
+        }
+      }
+      pthread_mutex_unlock(&doc_lock);
+    } else {
+      if (cmd[0] == 'g') {
+        int line_index = (cmd[8]-'0')*10 + (cmd[9]-'0');
+        pthread_mutex_lock(&doc_lock);
+        for (int i = 0; i < 256; i++) {
+          rtn[i] = document[line_index][i];
+          if (document[line_index][i] == '\0') {
+            break;
+          }
+        }
+        pthread_mutex_unlock(&doc_lock);
+        write(client_sockfd, &rtn, sizeof(rtn));
+      } else {
+        close(client_sockfd);
+      }
+    }
+    pthread_mutex_unlock(&cmd_lock);
 	}
+}
+void* user(){
+
 }
