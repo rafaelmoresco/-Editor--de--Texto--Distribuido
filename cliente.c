8eec497baca2d089f6afc9f7e100a8fb63304991
Rafael Moresco <rafaelmorescovieira@gmail.com>
2023-03-21 11:23:07 -0300

    remoção comentario

--- cliente.c
+++ cliente.c
@@ -18,8 +18,7 @@ int main()
 
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = inet_addr("127.0.0.1");
-  //address.sin_addr.s_addr = inet_addr("150.162.65.154");
-  //address.sin_addr.s_addr = htonl(INADDR_ANY);
+  //address.sin_addr.s_addr = inet_addr("");
   address.sin_port = htons(9734);
 
   len = sizeof(address);
